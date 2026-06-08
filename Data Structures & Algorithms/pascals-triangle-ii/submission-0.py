74817044f87116cb8cd4a8d65da3cf277853666e
Mayur Pophale <89771268+mayurpophale@users.noreply.github.com>
2026-06-08 16:40:32 +0530

    Add: pascals-triangle-ii - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for k in range(1, rowIndex + 1):
+            val = row[k - 1] * (rowIndex - k + 1) // k
+            row.append(val)
+
+        return row
